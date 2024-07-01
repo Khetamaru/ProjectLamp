1f2796ed299bbacaf91465a372110b6aafe75de4
Khetamaru <lasserre@intechinfo.fr>
2024-07-01 20:30:50 +0200

    Project creation

---
+++ ProjetLamp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetLamp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
